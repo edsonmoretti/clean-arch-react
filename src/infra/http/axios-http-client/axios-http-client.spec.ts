fea557b0a0bd13d077a993fe80791bec95b7993d
Edson Moretti do Nascimento <edsonmoretti@live.com>
2022-05-16 19:56:17 -0300

    test: add to call the correct verb

--- src/infra/http/axios-http-client/axios-http-client.spec.ts
+++ src/infra/http/axios-http-client/axios-http-client.spec.ts
@@ -1,6 +1,7 @@
 import {AxiosHttpClient} from "@/infra/http/axios-http-client/axios-http-client";
 import axios from "axios";
 import faker from "faker";
+import {HttpPostParams} from "@/data/protocols/http";
 
 jest.mock('axios');
 const mockedAxios = axios as jest.Mocked<typeof axios>;
@@ -9,11 +10,18 @@ const makeSut = (): AxiosHttpClient => {
   return new AxiosHttpClient();
 };
 
+const mockPostRequest = (): HttpPostParams<any> => {
+  return {
+    url: faker.internet.url(),
+    body: faker.random.objectElement()
+  }
+};
+
 describe('AxiosHttpClient', () => {
-    test('Should call axios with correct URL and verb', async () => {
-       const url = faker.internet.url();
-       const sut = makeSut();
-       sut.post({url: url});
-       expect(mockedAxios.post).toHaveBeenCalledWith(url);
+    test('Should call axios with correct values', async () => {
+        const request = mockPostRequest();
+        const sut = makeSut();
+        await sut.post(request);
+        expect(mockedAxios.post).toHaveBeenCalledWith(request.url, request.body);
     });
 });
